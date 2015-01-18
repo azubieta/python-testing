2f89c80024a459177e03fa208bb1d9a005751c64
Alexis López Zubieta <azubieta@uci.cu>
2015-01-18 14:55:01 -0500

    Configuring app.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='salesbroker',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Alexis',
+      author_email='azubieta90@gmail.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
       install_requires=['Flask>=0.7.2', 'MarkupSafe'],
      )
